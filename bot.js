3e92b2c884f7420b5428fb0d9d60bbf9c2972b21
DyniowyZabujaka <sok.pl@interia.pl>
2018-07-07 16:12:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,20 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'test') {
-    	message.reply('test');
-  	}
-});
+client.on('message', async message => {
+    if (message.content.startsWith('$clear')) {
+     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":potato: Nie masz uprawnien :) :potato:")
+        const args = message.content.slice(1).trim().split(/ +/g);
+        console.log(args);
+        const messagecount = parseInt(args[1], 10);
+        console.log(messagecount);
+        if (!messagecount || messagecount < 2 || messagecount > 200)
+            return message.reply('Minimalna liczba wyczyszczenia to 2');
+        let fetchmessage = await message.channel.fetchMessages({ count: messagecount });
+        message.channel.bulkDelete(messagecount)
+        message.channel.sendMessage("**Usunięto** "+messagecount)
+    }
+}); 
 
 client.on('message', message => {
   if (message.author.bot) return;
@@ -18,15 +27,104 @@ client.on('message', message => {
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
   let args = message.content.split(" ").slice(1);
-  if (command == "$say") {
+  if (command == "say") {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie posiadasz uprawnienia MANAGE_MESSAGES")
    message.delete(1);
    message.channel.sendMessage(args.join("  "))
   }
 });
 
-client.on("ready", () => {
-    client.user.setGame("Życie", "https://www.twitch.tv/vami12");
+client.on("guildMemberAdd", member => {
+  let guild = member.guild;
+    guild.defaultChannel.sendMessage(`:eggplant:**${member}** Właśnie dołączył na serwer! Zerknij do regulaminu i baw się dobrze. :eggplant:`);
+    member.user.sendFile('https://i.imgur.com/V5vQrF3.png', 'TwojaStara.png', 'testmessage');
+    member.user.sendMessage(`Witaj na serwerze **#TeamBakłażan** :hand_splayed:\n\nZalecamy zapoznać się z kanałem **#zasady-info**\n\nJako serwer oferujemy miłą atmosfere jak i częste konkursy.\n\nCała administracja życzy dobrej zabawy.\n\nhttps://discord.gg/tzEbMpq`);
 });
 
+client.on("guildMemberRemove", member => {
+  let guild = member.guild;
+    guild.defaultChannel.sendMessage(`:rage: **${member.user.username}** Opuścił nasze grono [*] :rage:`);
+    
+});
+
+client.on('message', message => {
+  if (message.content.startsWith(prefix + "flip")) {
+    const coin = ['Orzeł','Reszka'];
+    const random = Math.floor(Math.random()*2);
+    message.channel.send("**Wypadło na** | "+coin[random]);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === prefix + 'serverinfo') {
+    message.channel.send({embed: {
+  color: 3447003,
+  description: (`**Nazwa serwera**: ${message.guild.name}\n\n**Wszystkie osoby**: ${message.guild.memberCount}\n\n**Data utworzenia**: ${message.guild.createdAt}\n\n**ID**: ${message.guild.id}\n\n**Role**: ${message.guild.roles.size}\n\n**Właściciel**: ${message.guild.owner}`),
+  title: " ",
+  author: {
+  timestamp: new Date(),
+  footer: {
+  icon_url: client.user.avatarURL,
+  text: "© Example"
+    },
+  name: message.guild.name, 
+  icon_url: message.guild.iconURL
+    },
+  image: {
+  "url": " ",
+    },
+  thumbnail: {
+  "url": message.guild.iconURL
+    },
+}});
+  }
+});
+
+client.on('message', message => {
+  if (message.content === prefix + 'ping') {
+    message.channel.send({embed: {
+  color: 101981,
+  description: (':ping_pong:  `' + `${Date.now() - message.createdTimestamp}` + ' ms`'),
+  title: " ",
+  author: {
+  name: "Twój ping to", 
+  icon_url: message.author.avatarURL
+    },
+  image: {
+  "url": " ",
+    },
+  thumbnail: {
+  "url": " "
+    },
+}});
+  }
+});
+
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+    if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "lenny")) {
+    message.channel.sendMessage("( ͡° ͜ʖ ͡°)");
+    message.react(":lenny:304276195594207232");
+  } else
+  if (message.content.startsWith(prefix + "franio")) {
+    message.channel.sendFile('https://i.imgur.com/FHxjllM.png', 'TwojaStara.png', 'testmessage');
+    message.react("franciszek:383357843799277568")
+  } else
+  if (message.content.startsWith(prefix + "help")) {
+    message.react("oko:325409745278337024");
+    message.channel.sendMessage(":mailbox: Sprawdz swoją skrzynke odbiorczą");
+    message.author.sendMessage("chuj ci w dupe XD");
+  }
+
+});
+
+// Set the bot's "Playing: " status (must be in an event!)
+client.on("ready", () => {
+    client.user.setGame("Życie", "https://www.twitch.tv/vami12");
+                        
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
