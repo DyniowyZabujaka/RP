92c2b42e970ddad89b8607056672ec00e46a2023
DyniowyZabujaka <sok.pl@interia.pl>
2018-07-08 15:36:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,9 +16,17 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content.startsWith('* szuka ')) {
-    const coin = [' Znalazłeś',' Nie znalazłeś'];
-    const random = Math.floor(Math.random()*2);
-    message.channel.send(" :rose: "  +message.author   + coin[random]);
+    const coin2 = [' Znalazłeś',' Nie znalazłeś'];
+    const random2 = Math.floor(Math.random()*2);
+    message.channel.send(" :rose: "  +message.author   + coin2[random2]);
+  }
+});
+
+client.on('message', message => {
+  if (message.content.startsWith('* szukać ')) {
+    const coin3 = [' Znalazłeś',' Nie znalazłeś'];
+    const random3 = Math.floor(Math.random()*2);
+    message.channel.send(" :rose: "  +message.author   + coin3[random3]);
   }
 });
 
