0a2c89fd7299147f892e8492c4f9985e03e9bf1e
DyniowyZabujaka <sok.pl@interia.pl>
2018-11-12 23:55:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,33 @@ client.on("message", async message => {
 }
 });
 
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.indexOf(prefix) !== 0) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Let's go with a few common example commands! Feel free to delete or change those.
+
+    if (command === "nigdytegoniezgadniesz913xdjgdsfesfgdsadardw") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o => { });
+        // And we get the bot to say the thing:
+        const embed = {
+            "title": "🕶 **Wzór na postać**",
+            "description": `${sayMessage}`,
+            "color": 2421254
+        };
+        message.channel.send({ embed });
+}
+});
+
 client.on('message', message => {
   if (message.content.startsWith('*Strzela ')) {
     const coinq = [' Trafiłeś',' Nie trafiłeś'];
