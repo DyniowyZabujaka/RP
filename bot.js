e1884c11662fab21a126e8f93951152cadd4cc5d
DyniowyZabujaka <sok.pl@interia.pl>
2018-11-13 12:13:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,33 @@ client.on("message", async message => {
 
     // Let's go with a few common example commands! Feel free to delete or change those.
 
+    if (command === "ogloszenie") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o => { });
+        // And we get the bot to say the thing:
+        const embed = {
+            "title": ":bell: OGŁOSZENIE",
+            "description": `${sayMessage}`,
+            "color": 16777215
+        };
+        message.channel.send({ embed });
+}
+});
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.indexOf(prefix) !== 0) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Let's go with a few common example commands! Feel free to delete or change those.
+
     if (command === "nigdytegoniezgadniesz913xdjgdsfesfg") {
         // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
         // To get the "message" itself we join the `args` back into a string with spaces: 
